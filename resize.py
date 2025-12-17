009feba5b72d820dd1785d5227ba7cf2419e4e41
Frederik De Bleser <frederik@debleser.be>
2025-12-17 14:01:28 +0100

    New resize script

--- resize.py
+++ resize.py
@@ -1,13 +1,16 @@
+import os
 from PIL import Image
 from glob import glob
 import os
 from tqdm import tqdm
 
-
-image_paths = glob("datasets/yes-to-the-dress-paired/*.png")
+os.makedirs("datasets/yes-to-the-dress-256", exist_ok=True)
+image_paths = glob("datasets/yes-to-the-dress/*.png")
 for image_path in tqdm(image_paths):
-    #print(image_path)
+    # print(image_path)
     img = Image.open(image_path)
-    img = img.crop((0, 0, 512, 512))
-    dst_path = os.path.join("datasets/yes-to-the-dress", os.path.basename(image_path))
+    img = img.resize((256, 256))
+    dst_path = os.path.join(
+        "datasets/yes-to-the-dress-256", os.path.basename(image_path)
+    )
     img.save(dst_path)
